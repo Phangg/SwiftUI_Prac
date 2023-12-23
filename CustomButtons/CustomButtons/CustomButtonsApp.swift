6e3df6cbb45f0335b2d670e5cf676c636f229e4c
Phangg <leeckdwns9305@gmail.com>
2023-12-23 21:54:08 +0900

    Create: Start Project

---
+++ CustomButtons/CustomButtons/CustomButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonsApp.swift
+//  CustomButtons
+//
+//  Created by phang on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
