fc8184878bb115ac336692c561985a71460aa0c4
Phangg <leeckdwns9305@gmail.com>
2023-12-27 22:17:28 +0900

    Create: start project

---
+++ CustomProgressView/CustomProgressView/CustomProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomProgressViewApp.swift
+//  CustomProgressView
+//
+//  Created by phang on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
