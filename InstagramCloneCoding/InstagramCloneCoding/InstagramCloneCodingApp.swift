b1a99e8ecaaf3deb82678aafa659067db5ed850a
Phangg <leeckdwns9305@gmail.com>
2023-12-27 23:17:05 +0900

    Create: start project

---
+++ InstagramCloneCoding/InstagramCloneCoding/InstagramCloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneCodingApp.swift
+//  InstagramCloneCoding
+//
+//  Created by phang on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
